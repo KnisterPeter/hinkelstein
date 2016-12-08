c4b753e32c3b83cd390bbe500331048d006656f3
Markus Wolf <mail@markus-wolf.de>
2016-12-08 17:30:19 +0100

    refactor(hinkelstein): favor async-await over promises

--- src/foreach.ts
+++ src/foreach.ts
@@ -1,19 +1,18 @@
 import * as commonTags from 'common-tags';
 
-export function forEach<T>(list: T[], task: (task: T) => boolean | T | Promise<boolean | T>): Promise<T> {
-  return list.reduce((promise, entry) => {
-    return promise.then(continueReduce => {
-      if (continueReduce === false) {
-        console.log(`\n${commonTags.stripIndent`
-          -------------------------------------------------------------------------------
+export function forEach<T, R>(list: T[], task: (task: T) => boolean|R|Promise<boolean|R>): Promise<boolean|R> {
+  return list.reduce(async (promise, entry) => {
+    const continueReduce = await promise;
+    if (continueReduce === false) {
+      console.log(`\n${commonTags.stripIndent`
+        -------------------------------------------------------------------------------
 
-            Skipping ${entry}
+          Skipping ${entry}
 
-          -------------------------------------------------------------------------------
-        `}\n`);
-        return false;
-      }
-      return task(entry);
-    });
-  }, Promise.resolve(true as any));
+        -------------------------------------------------------------------------------
+      `}\n`);
+      return false;
+    }
+    return task(entry);
+  }, Promise.resolve(true));
 }
